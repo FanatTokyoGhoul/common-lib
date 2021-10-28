3c51bc5a0e759633147f5a16d26a9626971ea3a5
FanatTokyoGhoul <nayzer322@gmail.com>
2021-10-28 16:50:34 +0300

    Press ctrl+alt+L.

--- src/main/java/common/lib/configuration/MultiApplicationsConfig.java
+++ src/main/java/common/lib/configuration/MultiApplicationsConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class MultiApplicationsConfig {
     @Bean
-    public FilterRegistrationBean<PingFilter> loggingFilter(){
+    public FilterRegistrationBean<PingFilter> loggingFilter() {
         FilterRegistrationBean<PingFilter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
         filterFilterRegistrationBean.setFilter(new PingFilter());
         filterFilterRegistrationBean.addUrlPatterns("/ping");
